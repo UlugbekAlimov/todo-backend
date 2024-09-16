f386757f5f68a4ec56670d7c7e06f6c1ab5a9957
Улугбек Алимов <alimovu966@gmail.com>
2024-09-16 16:19:45 +0500

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
+  const config = new DocumentBuilder()
+    .setTitle('Todo backend')
+    .setDescription('todo app')
+    .setVersion('1.0')
+    .addTag('todo')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.enableCors();
-  
+
   await app.listen(3000);
 }
 bootstrap();
