8b061699fffcb9c29951c078b76493adb473e6c2
Улугбек Алимов <alimovu966@gmail.com>
2024-09-16 18:41:41 +0500

    env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://alimovu966:mongo2004@cluster0.ep58b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    ),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI), 
     TodoModule,
   ],
   controllers: [AppController],
